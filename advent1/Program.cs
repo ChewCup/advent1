4e28e3f357a9c06cef5b8314ee32f253c0df96e6
Viktor C <vikkez_@hotmail.com>
2021-12-07 19:39:37 +0100

    part 2 done

--- advent1/Program.cs
+++ advent1/Program.cs
@@ -11,7 +11,7 @@ namespace advent1
     {
         static void Main(string[] args)
         {
-            // ===== Day 1 Part 1 ===== 
+            //// ===== Day 1 Part 1 =====
 
             //int count = 0;
             //string[] lines = File.ReadAllLines(@"C:\Users\vikke\source\repos\advent\advent1\advent1\Text.txt");
@@ -30,7 +30,7 @@ namespace advent1
             //    }
             //}
 
-            // ===== Day 1 Part 2 =====
+            //// ===== Day 1 Part 2 =====
 
             //int count1 = 0;
             //for (int i = 0; i < numbers.Length - 3; i++)
@@ -43,46 +43,81 @@ namespace advent1
             //    }
             //}
 
-            // ===== Day 2 =====
+            //// ===== Day 2 Part 1 =====
 
-            string[] commands = File.ReadAllLines(@"C:\Users\vikke\source\repos\advent\advent1\advent1\commands.txt");
+            //string[] commands = File.ReadAllLines(@"C:\Users\vikke\source\repos\advent\advent1\advent1\commands.txt");
 
-            // Up = decreases depth
-            // Down = increases depth
+            //// Up = decreases depth
+            //// Down = increases depth
 
-            int ForwardX = 0, depth = 0;
+            //int forwardX = 0, depth = 0, sum = 0;
 
-            for (int i = 0; i < commands.Length; i++)
+            //for (int i = 0; i < commands.Length; i++)
+            //{
+            //    string[] splitWord = commands[i].Split(' ');
+
+            //    if (splitWord[0] == "forward")
+            //    {
+            //        int forwardVal = Convert.ToInt32(splitWord[1]);
+
+            //        forwardX += forwardVal;
+            //    }
+
+            //    if (splitWord[0] == "down")
+            //    {
+            //        int downVal = Convert.ToInt32(splitWord[1]);
+            //        depth += downVal;
+            //    }
+
+            //    if (splitWord[0] == "up")
+            //    {
+            //        int upVal = Convert.ToInt32(splitWord[1]);
+            //        depth -= upVal;
+            //    }
+            //    sum = forwardX * depth;
+            //    Console.WriteLine(sum);
+            //}
+
+            // ===== Day 2 Part 2 =====
+
+            // down X increases aim
+            // up X decreases aim
+            // forward X It increases horizontal position, It increases depth by your aim multiplied by X ( depth = aim * forward X )
+
+            int aimValue = 0, forwardX = 0, depth = 0, tempDepth = 0;
+
+            string[] aimCommands = File.ReadAllLines(@"C:\Users\vikke\source\repos\advent\advent1\advent1\commands.txt");
+
+            for (int i = 0; i < aimCommands.Length; i++)
             {
-                string[] splitWord = commands[i].Split(' ');
-                if (splitWord[0] == "forward")
-                {
-                    int forwardVal = Convert.ToInt32(splitWord[1]);
+                string[] aim = aimCommands[i].Split(' ');
 
-                    ForwardX += forwardVal;
+                if (aim[0] == "forward")
+                {
+                    int forwardVal = Convert.ToInt32(aim[1]);
+                    forwardX += forwardVal;
+
+                    depth += aimValue * forwardVal;
                 }
 
-                if (splitWord[0] == "down")
+                if (aim[0] == "down")
                 {
-                    int downVal = Convert.ToInt32(splitWord[1]);
-                    depth += downVal;
+                    int downVal = Convert.ToInt32(aim[1]);
+                    aimValue += downVal;
                 }
-                if (splitWord[0] == "up")
+
+                if (aim[0] == "up")
                 {
-                    int upVal = Convert.ToInt32(splitWord[1]);
-                    depth -= upVal;
+                    int upVal = Convert.ToInt32(aim[1]);
+                    aimValue -= upVal;
                 }
-                Console.WriteLine($"forward = {ForwardX}");
-                int sum = ForwardX * depth;
+                int sum = depth * forwardX;
                 Console.WriteLine(sum);
             }
-            
+
             //Console.WriteLine(count);
             //Console.WriteLine(count1);
             Console.ReadLine();
         }
-        
-
-
     }
 }
